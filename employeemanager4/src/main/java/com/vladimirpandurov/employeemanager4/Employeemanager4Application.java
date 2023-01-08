346066cb51a40c61ff0edadcefeb2795cd2a32c2
panvla <pandvlado@gmail.com>
2023-01-08 18:13:27 +0100

    front end done

---
+++ employeemanager4/src/main/java/com/vladimirpandurov/employeemanager4/Employeemanager4Application.java
@@ -0,0 +1,29 @@
+package com.vladimirpandurov.employeemanager4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@SpringBootApplication
+public class Employeemanager4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Employeemanager4Application.class, args);
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("http://localhost:4200");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
+
+}
---
+++ employeemanager4/src/test/java/com/vladimirpandurov/employeemanager4/Employeemanager4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladimirpandurov.employeemanager4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Employeemanager4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
